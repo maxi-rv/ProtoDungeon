785d62a58132ea5dce3bfc05f15e29474fbb3ffa
Maxi RV <maxi-rv@users.noreply.github.com>
2020-01-31 16:11:38 -0300

    Testing the AudioManager
    (That one from the Brackeys tutorial)

---
+++ ProtoDungeon01/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+
+public class AudioManager : MonoBehaviour
+{
+    public Audio[] soundFxs;
+
+    void Awake()
+    {
+        foreach(Audio au in soundFxs)
+        {
+            au.source = gameObject.AddComponent<AudioSource>();
+            au.source.clip = au.clip;
+
+            au.source.volume = au.volume;
+            au.source.pitch = au.pitch;
+        }
+    }
+    
+    public void Play(string name)
+    {
+        Audio au = Array.Find(soundFxs, audio => audio.name == name);
+        au.source.Play();
+    }
+
+}
